e1c388a4e096e46da266bac2e7fd5969fe7fda67
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-08-06 01:21:52 +0530

    Compleate the basic website

---
+++ send_mail.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "m.sombitpramanik@gmail.com"; // Replace with your Gmail address
+    $headers = "resume_website@From: $email";
+
+    // Send the email
+    $mailSent = mail($to, $subject, $message, $headers);
+
+    if ($mailSent) {
+        echo "success"; // Return success response to the client
+    } else {
+        echo "error"; // Return error response to the client
+    }
+}
+?>
