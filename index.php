19d5fdbf6edad955ae5e8f6fcb990639d1041b55
Sombit Pramanik <98169079+Sombitpramanik@users.noreply.github.com>
2023-08-06 21:01:35 +0530

    Update index.php deexpose the password key

--- index.php
+++ index.php
@@ -343,7 +343,7 @@ if(isset($_POST['submit'])){
       $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
       $mail->Username   = 'it2238@cemk.ac.in';                     //SMTP username
-      $mail->Password   = 'xtrdwokpyaibcdkq';                               //SMTP password
+      $mail->Password   = 'Password';                               //SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
       $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
